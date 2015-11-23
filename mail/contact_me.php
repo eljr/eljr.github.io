c70e18852c15a85fdddce7ac11f112b84bef020c
eljr <montesit2014@gmail.com>
2015-11-23 01:50:54 -0800

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,40 +1,42 @@
 <?php
-if(isset($_POST['submit'])) 
-{
-$message=
-'Full Name:	'.$_POST['fullname'].'<br />
-Subject:	'.$_POST['subject'].'<br />
-Phone:	'.$_POST['phone'].'<br />
-Email:	'.$_POST['emailid'].'<br />
-Comments:	'.$_POST['comments'].'
-';
-    require "PHPMailer-master/class.phpmailer.php"; //include phpmailer class
-      
-    // Instantiate Class  
-    $mail = new PHPMailer();  
-      
-    // Set up SMTP  
-    $mail->IsSMTP();                // Sets up a SMTP connection  
-    $mail->SMTPAuth = true;         // Connection with the SMTP does require authorization    
-    $mail->SMTPSecure = "ssl";      // Connect using a TLS connection  
-    $mail->Host = "smtp.gmail.com";  //Gmail SMTP server address
-    $mail->Port = 465;  //Gmail SMTP port
-    $mail->Encoding = '7bit';
-    
-    // Authentication  
-    $mail->Username   = "montesit2014@gmail.com"; // Your full Gmail address
-    $mail->Password   = "10bella@"; // Your Gmail password
-      
-    // Compose
-    $mail->SetFrom($_POST['emailid'], $_POST['fullname']);
-    $mail->AddReplyTo($_POST['emailid'], $_POST['fullname']);
-    $mail->Subject = "New Contact Form Enquiry";      // Subject (which isn't required)  
-    $mail->MsgHTML($message);
  
-    // Send To  
-    $mail->AddAddress("montesit2014@gmail.com", "Mr. Example"); // Where to send it - Recipient
-    $result = $mail->Send();		// Send!  
-	$message = $result ? '<div class="alert alert-success" role="alert"><strong>Success!</strong>Message Sent Successfully!</div>' : '<div class="alert alert-danger" role="alert"><strong>Error!</strong>There was a problem delivering the message.</div>';  
-	unset($mail);
+// Email address verification
+function isEmail($email) {
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
+ 
+if($_POST) {
+ 
+    // Enter the email where you want to receive the message
+    $emailTo = 'contact.azmind@gmail.com';
+ 
+    $clientEmail = addslashes(trim($_POST['email']));
+    $subject = addslashes(trim($_POST['subject']));
+    $message = addslashes(trim($_POST['message']));
+    $antispam = addslashes(trim($_POST['antispam']));
+ 
+    $array = array('emailMessage' => '', 'subjectMessage' => '', 'messageMessage' => '', 'antispamMessage' => '');
+ 
+    if(!isEmail($clientEmail)) {
+        $array['emailMessage'] = 'Invalid email!';
+    }
+    if($subject == '') {
+        $array['subjectMessage'] = 'Empty subject!';
+    }
+    if($message == '') {
+        $array['messageMessage'] = 'Empty message!';
+    }
+    if($antispam != '12') {
+        $array['antispamMessage'] = 'Wrong antispam answer!';
+    }
+    if(isEmail($clientEmail) && $subject != '' && $message != '' && $antispam == '12') {
+        // Send email
+        $headers = "From: " . $clientEmail . " <" . $clientEmail . ">" . "\r\n" . "Reply-To: " . $clientEmail;
+        mail($emailTo, $subject . " (bootstrap contact form tutorial)", $message, $headers);
+    }
+ 
+    echo json_encode($array);
+ 
+}
+ 
 ?>
