39c9cd0a15daabce63b04a653f0ca1e751b01eea
eljr <montesit2014@gmail.com>
2015-11-28 18:01:13 -0800

    bnews

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,42 +1,26 @@
 <?php
- 
-// Email address verification
-function isEmail($email) {
-    return filter_var($email, FILTER_VALIDATE_EMAIL);
-}
- 
-if($_POST) {
- 
-    // Enter the email where you want to receive the message
-    $emailTo = 'montesit2014@gmail.com';
- 
-    $clientEmail = addslashes(trim($_POST['email']));
-    $subject = addslashes(trim($_POST['subject']));
-    $message = addslashes(trim($_POST['message']));
-    $antispam = addslashes(trim($_POST['antispam']));
- 
-    $array = array('emailMessage' => '', 'subjectMessage' => '', 'messageMessage' => '', 'antispamMessage' => '');
- 
-    if(!isEmail($clientEmail)) {
-        $array['emailMessage'] = 'Invalid email!';
-    }
-    if($subject == '') {
-        $array['subjectMessage'] = 'Empty subject!';
-    }
-    if($message == '') {
-        $array['messageMessage'] = 'Empty message!';
-    }
-    if($antispam != '12') {
-        $array['antispamMessage'] = 'Wrong antispam answer!';
-    }
-    if(isEmail($clientEmail) && $subject != '' && $message != '' && $antispam == '12') {
-        // Send email
-        $headers = "From: " . $clientEmail . " <" . $clientEmail . ">" . "\r\n" . "Reply-To: " . $clientEmail;
-        mail($emailTo, $subject . " (bootstrap contact form tutorial)", $message, $headers);
-    }
- 
-    echo json_encode($array);
- 
-}
- 
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+	
+// Create the email and send the message
+$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
 ?>
